ed35080f3b95d32905e0be95c1fd2e98affce4c8
AlinaB <bernardezalina@gmail.com>
2024-04-16 10:57:50 +0200

    challenge

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,6 +9,6 @@ router.get('/ssr', TaskController.getAllSSR )
 router.get('/id/:_id', TaskController.getByID)
 router.put("/markascompleted/:_id", TaskController.updateCompleted)
 router.put('/id/:_id', TaskController.updateByName) 
-router.delete('/id/:_id', TaskController.deleteTask )
+router.delete('/delete/:_id', TaskController.deleteTask )
 
 module.exports = router
